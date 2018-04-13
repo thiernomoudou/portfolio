4ccbee51f351e63e7c2c75d6764455933608a6a5
Thierno Souleymane Diallo <souleymanemoudou@gmail.com>
2018-04-13 16:27:52 +0000

    Add image assets to the projects
    At the same time, fix dependency security error for the moment package

--- client/src/views/App/index.js
+++ client/src/views/App/index.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import persLogo from './../../images/pers_logo.svg';
+import mainImage from './../../images/main-image.svg'; 
+
 import './App.css';
 
 class App extends Component {
   render() {
+    let imgStyle = {
+        height: '600px'
+    };
+
+    let welcomeMessageStyle ={
+      height: '300px'
+    };
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header">     
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        < img src={mainImage}/>
+      
       </div>
     );
   }
