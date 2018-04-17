ed04ef3f5d0b4ca3fb963d69bc5cbf227653127d
Thierno Souleymane Diallo <souleymanemoudou@gmail.com>
2018-04-17 17:40:11 +0000

    Install Enzyme testing dependencies and write sample test

--- client/src/views/Home/__tests__/home.test.js
+++ client/src/views/Home/__tests__/home.test.js
@@ -1,13 +1,18 @@
+
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 
+import Navbar from './../../../components/Navbar/index';
+import Greeting from './../../../components/Greeting/index';
+import Banner from './../../../components/Banner/index';
+
 import HomePage from './../index';
 
-describe('Homepage', () => {
-    it('passing test', () => {
-        expect(true).toBeTruthy();
-    });
-    it('failing test', () => {
-        expect(2*3).toEqual(6);
+
+describe('Navbar', () => {
+    let wrapper;
+    it('has a logo', () => {
+        wrapper = shallow(<Navbar />);
+        expect(wrapper.find('.navbar-brand').length).toEqual(1);
     });
 });
