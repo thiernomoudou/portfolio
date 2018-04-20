4759b6b48bd019b567de3847ae537a8773e30929
Thierno Souleymane Diallo <souleymanemoudou@gmail.com>
2018-04-20 11:18:28 +0000

    Complete the Footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,13 +6,13 @@ const Footer = () => {
     return(
         <div className='footer text-center'>
             <div class="navbar-nav  navbar-expand-lg justify-content-center">
-                <a class="nav-item nav-link active" href="#">LinkedIn</a>
-                <a class="nav-item nav-link" href="#">Github</a>
-                <a class="nav-item nav-link" href="@gmail.com">Contact me</a>
+                <a class="nav-item nav-link active" target="_blank" href="https://www.linkedin.com/in/thierno-souleymane-diallo-440239137/">LinkedIn</a>
+                <a class="nav-item nav-link" target="_blank" href="https://github.com/thiernomoudou/">Github</a>
+                <a class="nav-item nav-link" href="mailto:tsdmoudou@gmail.com">Contact me</a>
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <p>© {year} <a href="">Thierno Souleymane Diallo</a>. All rights reserved.</p>
+                    <p>© {year} <a href="/">Thierno Souleymane Diallo</a>. All rights reserved.</p>
                 </div>
             </div>
         </div>
