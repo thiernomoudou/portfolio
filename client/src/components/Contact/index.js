ef49483f7897383222779c1e6440804924cca1cc
Thierno Souleymane Diallo <souleymanemoudou@gmail.com>
2018-04-25 17:32:21 +0000

    Adjust Contact page to allow text selection

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -8,7 +8,7 @@ const Contact = () => {
 
     let styles = {
         contactTitle: {
-            height:'15em',
+            height:'12em',
         },
 
         leftMount:{
@@ -24,14 +24,14 @@ const Contact = () => {
     
     return (
         <div className="contact-title">
-            <div className='row'>
-                <div className="col-md-3 d-none d-sm-none d-md-block">
+            <div className='row' >
+                <div className="col-md-3 d-none d-sm-none d-md-block" id="contact-banner">
                     <img src={mountain} style={styles.leftMount} className="img-fluid" alt="Greeting"/>
                 </div>
-                <div className="col-md-6 text-center">
+                <div className="col-md-6 text-center" id="contact-banner">
                     <img src={contactTitle} style={styles.contactTitle} className="img-fluid" alt="green-mountain"/>
                 </div>
-                <div className="col-md-3 d-none d-sm-none d-md-block">
+                <div className="col-md-3 d-none d-sm-none d-md-block" id="contact-banner">
                     <img src={mountain} style={styles.rightMount} className="img-fluid" alt="green-mountain"/>
                 </div>
             </div>
