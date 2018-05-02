b55561bd522579873788e6ab388f9167e0092efa
Thierno Souleymane Diallo <souleymanemoudou@gmail.com>
2018-05-02 13:05:06 +0000

    Implement Sending email with sendgrid

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const sgMail = require('@sendgrid/mail');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 
 app.set("port", process.env.PORT || 3001);
 
@@ -19,14 +24,42 @@ if (process.env.NODE_ENV === "production") {
 app.get("/welcome", (req, res) => {
     res.send("Welcome to my site");
   });
-
+ls
 app.post("/sending_email", (req, res) => {
+
+  let key = process.env.SENDGRID_API_KEY;
+  let username   = process.env.SENDGRID_USERNAME;
+  // let password   = process.env.SENDGRID_PASSWORD;
+  let name = req.body.name;
+  let fromEmail = req.body.email;
+  let text = req.body.text;
+  let subjectText = `Colloboration inquiry from ${name}`;
+  let toEmail = [{email: 'souleymanemoudou@gmail.com'}]
+
   res.setHeader('Content-Type', 'application/json');
-  res.send({
-    name: req.body.name || null,
-    email: req.body.email || null,
-    text: req.body.text || null
-});
+  
+  sgMail.setApiKey(key); 
+
+  const msg = {
+    to: toEmail,
+    from: fromEmail,
+    subject: subjectText,
+    text: text,
+    // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+
+  sgMail.send(msg, function(error, info){
+    console.log(key);
+    console.log(username);
+    if(error){
+        console.log(error);
+        res.json({yo: 'error'});
+    }else{
+        console.log('Message sent: ' + info);
+        res.json(info);
+    };
+  });
+  
 });
 
 app.listen(app.get("port"), () => {
