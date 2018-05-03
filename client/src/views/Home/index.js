d3355c7cbe6a1a281f2df416b11605e77c3ed78b
Thierno Souleymane Diallo <souleymanemoudou@gmail.com>
2018-05-03 15:57:39 +0000

    Implement page title change with react-helmet

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Helmet} from 'react-helmet';
 
 import Navbar from './../../components/Navbar/index';
 import Greeting from './../../components/Greeting/index';
@@ -12,6 +13,11 @@ export default class HomePage extends Component{
     render() {
         return(
             <div>
+                <Helmet>
+                    <title>Home page Thierno Souleymane Diallo</title>
+                    <meta name="author" content="Thierno Souleymane Diallo" />
+                    <meta name="description" content="Thierno Souleymane Diallo, Guinean Web Developer passionnate about IT and Entrepreunership" />
+                </Helmet>   
                 <Navbar />
                 <Greeting />
                 <Banner />
