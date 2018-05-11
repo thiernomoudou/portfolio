171a4b92777aa90ff8dfcd3fe378b6ae06798663
Thierno Souleymane Diallo <souleymanemoudou@gmail.com>
2018-05-11 16:44:21 +0000

    Add some mobile features optimization

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -4,36 +4,74 @@ import { Link } from 'react-router-dom';
 import persLogo from './../../images/pers_logo.svg';
 
 
-function Navbar(props){
+class Navbar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            stick : 'sticky-top'
+        };
+    }
 
-    let styles ={
-        logo: {
-            height:'60px'
-        },
-        font :{
-            color: 'red'
+    /**
+   * Calculate & Update state of new dimensions
+   */
+    updateDimensions = () => {
+        if(window.innerWidth < 750) {
+            this.setState({ stick: ''});
+        }else{
+            this.setState({stick: 'sticky-top'})
         }
-    };
-    return(
-        <div className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand app-logo" to="/">
-                <img src={[persLogo]} style={styles.logo} alt="" />
-            </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" 
-            data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" 
-            aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                <div className="navbar-nav" id='navbar-right' >
-                    <Link className="nav-item nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
-                    <Link className="nav-item nav-link" to="/projects">Projects</Link>
-                    <Link className="nav-item nav-link"  to="/blog">Blog</Link>
-                    <Link className="nav-item nav-link" to="/contact">Contact</Link>
+    }
+
+    componentWillMount() {
+        this.updateDimensions();
+    }
+
+    /**
+   * Add event listener
+   */
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    /**
+   * Remove event listener
+   */
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+    
+    
+    render(){
+        let styles ={
+            logo: {
+                height:'60px'
+            },
+            font :{
+                color: 'red'
+            }
+        };
+        return(
+            <div className={"navbar navbar-expand-lg navbar-light bg-light " + this.state.stick }>
+                <Link className="navbar-brand app-logo" to="/">
+                    <img src={[persLogo]} style={styles.logo} alt="" />
+                </Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" 
+                data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" 
+                aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav" id='navbar-right' >
+                        <Link className="nav-item nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
+                        <Link className="nav-item nav-link" to="/projects">Projects</Link>
+                        <Link className="nav-item nav-link"  to="/blog">Blog</Link>
+                        <Link className="nav-item nav-link" to="/contact">Contact</Link>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Navbar;
